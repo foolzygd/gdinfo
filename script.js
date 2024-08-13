dca0e725b3ed630ea338a359863018dc67e1a712
foolzygd <152037113+foolzygd@users.noreply.github.com>
2024-08-12 23:11:34 -0300

    Imported the website

---
+++ script.js
@@ -0,0 +1,28 @@
+async function robob() {
+    try {
+        const lvlid = document.getElementById("lvlid");
+        const lvl = lvlid.value;
+
+        const response = await fetch('http://www.boomlings.com/database/downloadGJLevel22.php', {
+            method: 'POST',
+            headers: {
+                'User-Agent': '',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                levelID: lvl,
+                secret: 'Wmfd2893gb7'
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.text();
+        const vals = result.split(":")
+        console.log(vals);
+    } catch (error) {
+        console.error(error);
+    }
+}
